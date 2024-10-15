52a021ef9eaa459f03488f8a29793869a01d92ce
panchenkoiryna <145868222+panchenkoiryna@users.noreply.github.com>
2024-10-15 14:33:54 +0300

    Changed keyboard arrows to buttons

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./App.css";
+
 const images = [
   "https://koruna.ua/en/wp-content/uploads/sites/3/2018/04/bukovel.jpg",
   "https://ukrainetrek.com/blog/wp-content/uploads/2011/12/ukrainian-carpathians-landscape-1.jpg",
@@ -8,18 +10,47 @@ const images = [
   "https://www.mountainiq.com/wp-content/uploads/2019/02/Ladovy-stit.jpg?x83495",
   "https://www.uncover-romania.com/wp-content/uploads/2018/02/taul-tapului-retezat-mountains-1.jpg"
 ];
+
 function App() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const handleNext = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === images.length - 1 ? 0 : prevSlide + 1
+    );
+  };
+
+  const handlePrev = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === 0 ? images.length - 1 : prevSlide - 1
+    );
+  };
+
   return (
     <div className="box">
-      <Carousel useKeyboardArrows={true} infiniteLoop={true}>
+      <Carousel
+        selectedItem={currentSlide}
+        showArrows={false} 
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+        showIndicators={false}
+      >
         {images.map((URL, index) => (
-          <div className="slide">
-            <img alt="sample_file" src={URL} key={index} />
+          <div className="slide" key={index}>
+            <img alt="sample_file" src={URL} />
           </div>
         ))}
       </Carousel>
+
+      <div className="buttons">
+        <button className="button" onClick={handlePrev}>Вліво</button>
+        <button className="button" onClick={handleNext}>Вправо</button>
+      </div>
     </div>
   );
 }
+
 export default App;
+
 
